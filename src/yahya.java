191738c6cc978f4b6d49d458262ee60b62da2cf0
Lenovo <saputrayahya0607@gmail.com>
2023-11-13 09:39:19 +0700

    file yahya

--- src/yahya.java
+++ src/yahya.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class yahya {
     public static void main(String[] args) {
-        if ("hello")
+        Scanner S = new Scanner(System.in);
+        System.out.println("masukan angka : ");
+        int x = S.nextInt();
+        if (x > 0) {
+            System.out.println(x + " adalah bilangan positif");
+        }
+        else {
+            if (x > 0) {
+                System.out.println(x + " adalah bilangan negatif");
+            } else {
+                System.out.println(x + "adalah angka nol");
+            }
+        }
     }
 }
